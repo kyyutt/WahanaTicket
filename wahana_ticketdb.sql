a812723437a39280372950c83acdb72ea757bfb5
kyyutt <riskiyahkya@gmail.com>
2025-06-16 20:16:52 +0900

    update sql

--- wahana_ticketdb.sql
+++ wahana_ticketdb.sql
@@ -1,116 +1,159 @@
--- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
---
--- Host: localhost    Database: wahana_ticketdb
--- ------------------------------------------------------
--- Server version	9.0.1
+-- Database: wahana_ticketdb
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+DROP TABLE IF EXISTS sales;
 
---
--- Table structure for table `sales`
---
+DROP TABLE IF EXISTS tickets;
 
-DROP TABLE IF EXISTS `sales`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `sales` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `ticket_id` int NOT NULL,
-  `user_id` int NOT NULL,
-  `quantity` int NOT NULL,
-  `total_price` decimal(10,2) NOT NULL,
-  `sold_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `ticket_id` (`ticket_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `sales_ibfk_1` FOREIGN KEY (`ticket_id`) REFERENCES `tickets` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `sales_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS users;
 
---
--- Dumping data for table `sales`
---
+-- Table users
+CREATE TABLE users (
+    id int NOT NULL AUTO_INCREMENT,
+    username varchar(50) NOT NULL,
+    password varchar(255) NOT NULL,
+    role enum('admin', 'kasir') NOT NULL,
+    created_at datetime DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    UNIQUE KEY username (username)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
-LOCK TABLES `sales` WRITE;
-/*!40000 ALTER TABLE `sales` DISABLE KEYS */;
-INSERT INTO `sales` VALUES (2,2,2,1,132331.00,'2025-06-13 17:42:30','2025-06-13 18:41:39'),(3,2,2,3,396993.00,'2025-06-13 18:06:10','2025-06-13 18:41:39'),(4,2,2,1,132331.00,'2025-06-13 18:34:12','2025-06-13 18:41:39'),(5,2,2,1,132331.00,'2025-06-13 18:36:46','2025-06-13 18:41:39'),(6,2,4,2,264662.00,'2025-06-13 22:05:35','2025-06-13 13:05:35'),(7,2,2,1,132331.00,'2025-06-13 22:06:11','2025-06-13 13:06:11'),(8,4,4,2,30000.00,'2025-06-13 22:29:09','2025-06-13 13:29:09');
-/*!40000 ALTER TABLE `sales` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO
+    users
+VALUES (
+        1,
+        'admin',
+        '$2y$10$nZp0ilkXO723.hYt73LKQOaeVuvQikiyYpIjc8MVUAgfBH3PksTtO',
+        'admin',
+        '2025-06-13 08:04:43'
+    ),
+    (
+        2,
+        'Kasir2',
+        '$2y$10$Ixj8Ti83oGs9E2NJ1dEkeOwYAqCoqtjZgmXMYgy2YTferHmVl5PuW',
+        'kasir',
+        '2025-06-13 09:05:43'
+    ),
+    (
+        4,
+        'Kasir1',
+        '$2y$10$hbh4FniQpA4raI.9nk5.9OI2LrlH4DuEhiF6jo8F9a8WGSlqPzq1a',
+        'kasir',
+        '2025-06-13 21:58:58'
+    ),
+    (
+        5,
+        'Kasir3',
+        '$2y$10$LE6jq/Zt5wpl7NpDgS533Oh9VfEuIpJnArcN21kIPjS.RRvWboo9C',
+        'kasir',
+        '2025-06-13 22:28:28'
+    );
 
---
--- Table structure for table `tickets`
---
+-- Table tickets
+CREATE TABLE tickets (
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(100) NOT NULL,
+    price decimal(10, 2) NOT NULL,
+    stock int NOT NULL,
+    created_at datetime DEFAULT CURRENT_TIMESTAMP,
+    updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
-DROP TABLE IF EXISTS `tickets`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tickets` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `price` decimal(10,2) NOT NULL,
-  `stock` int NOT NULL,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO
+    tickets
+VALUES (
+        2,
+        'sdf',
+        132331.00,
+        3,
+        '2025-06-13 15:52:36',
+        '2025-06-13 22:06:11'
+    ),
+    (
+        4,
+        'Anak anak',
+        15000.00,
+        88,
+        '2025-06-13 22:28:00',
+        '2025-06-13 22:29:09'
+    );
 
---
--- Dumping data for table `tickets`
---
+-- Table sales
+CREATE TABLE sales (
+    id int NOT NULL AUTO_INCREMENT,
+    ticket_id int NOT NULL,
+    user_id int NOT NULL,
+    quantity int NOT NULL,
+    total_price decimal(10, 2) NOT NULL,
+    sold_at datetime DEFAULT CURRENT_TIMESTAMP,
+    created_at datetime DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (ticket_id) REFERENCES tickets (id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
-LOCK TABLES `tickets` WRITE;
-/*!40000 ALTER TABLE `tickets` DISABLE KEYS */;
-INSERT INTO `tickets` VALUES (2,'sdf',132331.00,3,'2025-06-13 15:52:36','2025-06-13 22:06:11'),(4,'Anak anak',15000.00,88,'2025-06-13 22:28:00','2025-06-13 22:29:09');
-/*!40000 ALTER TABLE `tickets` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `role` enum('admin','kasir') NOT NULL,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'admin','$2y$10$nZp0ilkXO723.hYt73LKQOaeVuvQikiyYpIjc8MVUAgfBH3PksTtO','admin','2025-06-13 08:04:43'),(2,'Kasir2','$2y$10$Ixj8Ti83oGs9E2NJ1dEkeOwYAqCoqtjZgmXMYgy2YTferHmVl5PuW','kasir','2025-06-13 09:05:43'),(4,'Kasir1','$2y$10$hbh4FniQpA4raI.9nk5.9OI2LrlH4DuEhiF6jo8F9a8WGSlqPzq1a','kasir','2025-06-13 21:58:58'),(5,'Kasir3','$2y$10$LE6jq/Zt5wpl7NpDgS533Oh9VfEuIpJnArcN21kIPjS.RRvWboo9C','kasir','2025-06-13 22:28:28');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-06-13 22:44:13
+INSERT INTO
+    sales
+VALUES (
+        2,
+        2,
+        2,
+        1,
+        132331.00,
+        '2025-06-13 17:42:30',
+        '2025-06-13 18:41:39'
+    ),
+    (
+        3,
+        2,
+        2,
+        3,
+        396993.00,
+        '2025-06-13 18:06:10',
+        '2025-06-13 18:41:39'
+    ),
+    (
+        4,
+        2,
+        2,
+        1,
+        132331.00,
+        '2025-06-13 18:34:12',
+        '2025-06-13 18:41:39'
+    ),
+    (
+        5,
+        2,
+        2,
+        1,
+        132331.00,
+        '2025-06-13 18:36:46',
+        '2025-06-13 18:41:39'
+    ),
+    (
+        6,
+        2,
+        4,
+        2,
+        264662.00,
+        '2025-06-13 22:05:35',
+        '2025-06-13 13:05:35'
+    ),
+    (
+        7,
+        2,
+        2,
+        1,
+        132331.00,
+        '2025-06-13 22:06:11',
+        '2025-06-13 13:06:11'
+    ),
+    (
+        8,
+        4,
+        4,
+        2,
+        30000.00,
+        '2025-06-13 22:29:09',
+        '2025-06-13 13:29:09'
+    );
